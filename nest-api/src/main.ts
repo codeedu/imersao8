ec7d781642dc3f1d349514c355f9421e30378e62
Luiz Carlos <argentinaluiz@gmail.com>
2022-06-21 11:25:07 -0300

    add nestjs

---
+++ nest-api/src/main.ts
@@ -0,0 +1,34 @@
+import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.connectMicroservice({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: process.env.KAFKA_CLIENT_ID,
+        brokers: [process.env.KAFKA_BROKER],
+        ssl: true,
+        sasl: {
+          mechanism: 'plain', // scram-sha-256 or scram-sha-512
+          username: process.env.KAFKA_SASL_USERNAME,
+          password: process.env.KAFKA_SASL_PASSWORD,
+        },
+      },
+      consumer: {
+        groupId:
+          !process.env.KAFKA_CONSUMER_GROUP_ID ||
+          process.env.KAFKA_CONSUMER_GROUP_ID === ''
+            ? 'my-consumer-' + Math.random()
+            : process.env.KAFKA_CONSUMER_GROUP_ID,
+      },
+    },
+  });
+
+  await app.startAllMicroservicesAsync();
+  await app.listen(3000);
+}
+bootstrap();
---
+++ nest-api/src/routes/routes.module.ts
@@ -0,0 +1,43 @@
+import { Module } from '@nestjs/common';
+import { RoutesService } from './routes.service';
+import { RoutesController } from './routes.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Route, RouteSchema } from './entities/route.entity';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { RoutesGateway } from './routes.gateway';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Route.name, schema: RouteSchema }]),
+    ClientsModule.registerAsync([
+      {
+        name: 'KAFKA_SERVICE',
+        useFactory: (): any => ({
+          transport: Transport.KAFKA,
+          options: {
+            client: {
+              clientId: process.env.KAFKA_CLIENT_ID,
+              brokers: [process.env.KAFKA_BROKER],
+              ssl: true,
+              sasl: {
+                mechanism: 'plain', // scram-sha-256 or scram-sha-512
+                username: process.env.KAFKA_SASL_USERNAME,
+                password: process.env.KAFKA_SASL_PASSWORD,
+              },
+            },
+            consumer: {
+              groupId:
+                !process.env.KAFKA_CONSUMER_GROUP_ID ||
+                process.env.KAFKA_CONSUMER_GROUP_ID === ''
+                  ? 'my-consumer-' + Math.random()
+                  : process.env.KAFKA_CONSUMER_GROUP_ID,
+            },
+          },
+        }),
+      },
+    ]),
+  ],
+  controllers: [RoutesController],
+  providers: [RoutesService, RoutesGateway],
+})
+export class RoutesModule {}
